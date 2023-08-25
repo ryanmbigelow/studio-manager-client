9e670f86bfe19799fe863e39c0503b6aa0d17c7f
ryanmbigelow <ryanmbigelow@gmail.com>
2023-08-25 14:22:57 -0500

    set up user authentication. users can now log into the app via google

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { registerUser } from '../utils/auth'; // Update with path to registerUser
+import { registerUser } from '../utils/auth';
 
 function RegisterForm({ user, updateUser }) {
   const [formData, setFormData] = useState({
-    bio: '',
+    firstName: '',
+    lastName: '',
+    isAdmin: false,
+    profilePicture: '',
     uid: user.uid,
   });
 
@@ -18,12 +21,20 @@ function RegisterForm({ user, updateUser }) {
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+        <Form.Label>Customer Info</Form.Label>
+        <div>___________________</div>
+        {/* FIRST NAME  */}
+        <Form.Text className="text-muted">First Name</Form.Text>
+        <Form.Control as="textarea" name="firstName" required placeholder="Enter First Name" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        {/* LAST NAME  */}
+        <Form.Text className="text-muted">Last Name</Form.Text>
+        <Form.Control as="textarea" name="lastName" required placeholder="Enter Last Name" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        {/* PROFILE PICTURE  */}
+        <Form.Text className="text-muted">Profile Picture URL</Form.Text>
+        <Form.Control as="textarea" name="profilePicture" required placeholder="Enter an Image URL for your Profile Picture" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
       </Form.Group>
       <Button variant="primary" type="submit">
-        Submit
+        Register Account
       </Button>
     </Form>
   );
