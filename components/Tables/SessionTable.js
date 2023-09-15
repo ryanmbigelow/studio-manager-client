cd790fe12fc302e3d6b135bfbc08ba4cb719b174
ryanmbigelow <ryanmbigelow@gmail.com>
2023-09-15 14:34:18 -0500

    replaced session cards with a session table. revamped homepage to include an image, a description of the app, and an action button.

---
+++ components/Tables/SessionTable.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { deleteSession } from '../../utils/data/sessionData';
+
+const SessionTable = ({ sessionObj, onUpdate }) => {
+  const deleteThisSession = () => {
+    if (window.confirm('Are you sure you want to cacel this session?')) {
+      deleteSession(sessionObj.id).then(() => onUpdate());
+    }
+  };
+  return (
+    <tr>
+      <th scope="row">1</th>
+      <td>{sessionObj.artist}</td>
+      <td>{sessionObj.start_time}</td>
+      <td>{sessionObj.end_time}</td>
+      <td>{sessionObj.date}</td>
+      <td>{sessionObj.engineer_id.first_name} {sessionObj.engineer_id.last_name}</td>
+      <td>
+        <Link href={`/sessions/${sessionObj.id}`} passHref>
+          <Button variant="light" className="btn btn-outline-dark">View Session Details</Button>
+        </Link>
+      </td>
+      <td>
+        <Link href={`/sessions/edit/${sessionObj.id}`} passHref>
+          <Button variant="light" className="btn btn-outline-dark">Edit Session</Button>
+        </Link>
+      </td>
+      <td>
+        <Button variant="light" className="btn btn-outline-danger" onClick={deleteThisSession}>Cancel Session</Button>
+      </td>
+    </tr>
+  );
+};
+
+export default SessionTable;
+
+SessionTable.propTypes = {
+  sessionObj: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    artist: PropTypes.string.isRequired,
+    start_time: PropTypes.string.isRequired,
+    end_time: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    engineer_id: PropTypes.shape({
+      first_name: PropTypes.string.isRequired,
+      last_name: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 import { getAllSessions } from '../utils/data/sessionData';
-import SessionCard from '../components/Cards/SessionCard';
+import SessionTable from '../components/Tables/SessionTable';
+import mixingBoard from '../public/mixingBoard.jpeg';
 
 function Home() {
   const { user } = useAuth();
@@ -16,16 +18,39 @@ function Home() {
   }, [setSessions]);
   return (
     <div>
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <div className="sessionCards">
-        {sessions ? sessions.map((session) => (
-          <SessionCard key={`session--${session.id}`} sessionObj={session} onUpdate={getAllTheSessions} />
-        )) : 'No sessions booked'}
+      <div id="homepage-greeting">
+        <div id="homepage-text">
+          <h1>Welcome to Studio Manager, {user.fbUser.displayName}!
+          </h1>
+          <p>Studio Manager is an organizational tool for studio owners to manage both recording sessions and engineers. You can view your sessions below, create a new session by selecting &quot;Book a Session&quot; and manage your engineers in the &quot;Engineers&quot; tab.</p>
+          <Link passHref href="/sessions/new">
+            <Button>Book a Session</Button>
+          </Link>
+        </div>
+        <div className="padding" />
+        <Image id="mixing-board" src={mixingBoard} alt="mixing board" width="3008rem" height="2000" />
       </div>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+      <h4>Upcoming Sessions:</h4>
+      <table id="homepage-session-table" className="table table-striped table-bordered table-hover">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Artist</th>
+            <th scope="col">Start Time</th>
+            <th scope="col">End Time</th>
+            <th scope="col">Date</th>
+            <th scope="col">Booked By</th>
+            <th scope="col">View Details</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sessions ? sessions.map((session) => (
+            <SessionTable key={`session--${session.id}`} sessionObj={session} onUpdate={getAllTheSessions} />
+          )) : 'No sessions booked'}
+        </tbody>
+      </table>
     </div>
   );
 }
